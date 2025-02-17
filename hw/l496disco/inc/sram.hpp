902e04df7533b98974159fc8e87f1f1e771f39a0
Dmitry Ponyatov <dponyatov@gmail.com>
2025-02-17 11:36:54 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- hw/l496disco/inc/sram.hpp
+++ hw/l496disco/inc/sram.hpp
@@ -1,8 +1,21 @@
 #pragma once
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /// @defgroup sram sram
 /// @brief BSP: external memory (PSRAM)
-/// @ingroup stm32l496g
+/// @ingroup l496disco
 /// @{
+
+/// @name status
+/// @{
+#define SRAM_OK    0x00
+#define SRAM_ERROR 0x01
+/// @}
 /// @}
 
+#ifdef __cplusplus
+}
+#endif
