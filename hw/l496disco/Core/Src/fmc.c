afaec75ae349ec330193f00f300385facf84de6c
Dmitry Ponyatov <dponyatov@gmail.com>
2025-02-18 13:52:44 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- hw/l496disco/Core/Src/fmc.c
+++ hw/l496disco/Core/Src/fmc.c
@@ -46,14 +46,14 @@ void MX_FMC_Init(void)
   hsram1.Instance = FMC_NORSRAM_DEVICE;
   hsram1.Extended = FMC_NORSRAM_EXTENDED_DEVICE;
   /* hsram1.Init */
-  hsram1.Init.NSBank = FMC_NORSRAM_BANK2;
+  hsram1.Init.NSBank = FMC_NORSRAM_BANK2; //
   hsram1.Init.DataAddressMux = FMC_DATA_ADDRESS_MUX_DISABLE;
   hsram1.Init.MemoryType = FMC_MEMORY_TYPE_SRAM;
   hsram1.Init.MemoryDataWidth = FMC_NORSRAM_MEM_BUS_WIDTH_16;
   hsram1.Init.BurstAccessMode = FMC_BURST_ACCESS_MODE_DISABLE;
   hsram1.Init.WaitSignalPolarity = FMC_WAIT_SIGNAL_POLARITY_LOW;
   hsram1.Init.WaitSignalActive = FMC_WAIT_TIMING_BEFORE_WS;
-  hsram1.Init.WriteOperation = FMC_WRITE_OPERATION_DISABLE;
+  hsram1.Init.WriteOperation = FMC_WRITE_OPERATION_DISABLE; //
   hsram1.Init.WaitSignal = FMC_WAIT_SIGNAL_DISABLE;
   hsram1.Init.ExtendedMode = FMC_EXTENDED_MODE_DISABLE;
   hsram1.Init.AsynchronousWait = FMC_ASYNCHRONOUS_WAIT_DISABLE;
@@ -81,14 +81,14 @@ void MX_FMC_Init(void)
   hsram2.Instance = FMC_NORSRAM_DEVICE;
   hsram2.Extended = FMC_NORSRAM_EXTENDED_DEVICE;
   /* hsram2.Init */
-  hsram2.Init.NSBank = FMC_NORSRAM_BANK1;
+  hsram2.Init.NSBank = FMC_NORSRAM_BANK1; //
   hsram2.Init.DataAddressMux = FMC_DATA_ADDRESS_MUX_DISABLE;
   hsram2.Init.MemoryType = FMC_MEMORY_TYPE_SRAM;
   hsram2.Init.MemoryDataWidth = FMC_NORSRAM_MEM_BUS_WIDTH_16;
   hsram2.Init.BurstAccessMode = FMC_BURST_ACCESS_MODE_DISABLE;
   hsram2.Init.WaitSignalPolarity = FMC_WAIT_SIGNAL_POLARITY_LOW;
   hsram2.Init.WaitSignalActive = FMC_WAIT_TIMING_BEFORE_WS;
-  hsram2.Init.WriteOperation = FMC_WRITE_OPERATION_ENABLE;
+  hsram2.Init.WriteOperation = FMC_WRITE_OPERATION_ENABLE; ////////////
   hsram2.Init.WaitSignal = FMC_WAIT_SIGNAL_DISABLE;
   hsram2.Init.ExtendedMode = FMC_EXTENDED_MODE_DISABLE;
   hsram2.Init.AsynchronousWait = FMC_ASYNCHRONOUS_WAIT_DISABLE;
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
-fn add<T>(i: T, j: T) -> T {
-    i + j
-   }
+#![no_std]
+#![allow(dead_code)]
+
+/// `( -- )` empty command: do nothing
+fn nop() {}
+
+/// `( -- )` stop system until hard reset
+fn halt() {
+    loop {}
+}
+
+// fn add<T>(i: T, j: T) -> T {
+//     i + j
+//    }
