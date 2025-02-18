5091b31363c209390d5ee5adaeada3d58c93b9fc
Dmitry Ponyatov <dponyatov@gmail.com>
2025-02-18 14:47:34 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/vm.rs
+++ src/vm.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![allow(dead_code)]
 #![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
 
 /// D stack size
 const Dsz: usize = 0x10;
@@ -10,25 +11,31 @@ const Rsz: usize = 0x100;
 const Msz: usize = 0x1000;
 
 /// data stack
-static D: [i32; Dsz] = [0; Dsz];
+static mut D: [i32; Dsz] = [0; Dsz];
 /// D stack pointer
-static Dp: u8 = 0;
+static mut Dp: u8 = 0;
 
 /// return stack
-static R: [u32; Rsz] = [0; Rsz];
+static mut R: [u32; Rsz] = [0; Rsz];
 /// R stack pointer
-static Rp: u16 = 0;
+static mut Rp: u16 = 0;
 
 /// main memory
-static M: [u8; Msz] = [0; Msz];
+static mut M: [u8; Msz] = [0; Msz];
 /// compiler pointer
-static Cp: u16 = 0;
+static mut Cp: u16 = 0;
 /// instruction pointer
-static Ip: u16 = 0;
+static mut Ip: u16 = 0;
 
-// fn add<T>(i: T, j: T) -> T {
-//     i + j
-//    }
+/// VM command opcodes
+enum Op {
+    nop = 0x00,
+    halt = 0xff,
+    jmp = 0x01,
+    qjmp = 0x02,
+    call = 0x03,
+    ret = 0x04,
+}
 
 /// `( -- )` empty command: do nothing
 fn nop() {}
@@ -36,4 +43,17 @@ fn nop() {}
 /// `( -- )` stop system until hard reset
 fn halt() {
     loop {}
+}
+
+unsafe fn jmp(addr: u16) {
+    assert!((addr as usize) < Msz);
+    Ip = addr;
+}
+
+unsafe fn qjmp(addr: u16) {
+    assert!(Dp > 0);
+    Dp -= 1;
+    if D[Dp as usize] == 0 {
+        jmp(addr);
+    }
 }
