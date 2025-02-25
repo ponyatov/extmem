be3502a6a223af6d8b27a83471eea1f8331d4021
Dmitry Ponyatov <dponyatov@gmail.com>
2025-02-25 12:06:39 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cmake/mingw-w64-ucrt-x86_64.cmake
+++ cmake/mingw-w64-ucrt-x86_64.cmake
@@ -1,8 +1,9 @@
 set(CMAKE_SYSTEM_NAME  Windows)
 set(TOOLCHAIN_PREFIX   mingw-w64-ucrt-x86_64)
 
-include(cmake/any_toolchain.cmake)
+include(any_toolchain)
 
 add_compile_options(
     "-march=native"
+    "-mconsole"
 )
